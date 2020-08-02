5e3b018c1e52ab27b899892f3dbccac81763c09d
Sudarshan Gadiyar <sudarshangadiyar@gmail.com>
2020-08-02 19:16:54 +0530

    Second commit

--- practice1.sql
+++ practice1.sql
@@ -1,1 +1,23 @@
 create database practice;
+
+use	practice;
+
+create table employees(
+id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    age INT NOT NULL,
+    current_status VARCHAR(255) NOT NULL DEFAULT 'employed',
+    PRIMARY KEY(id)
+);
+
+insert into employees(first_name,last_name,age,id) values(
+'sudarshan','gadiyar',21,101);
+
+select * from employees;
+
+insert into employees(first_name,last_name,age) values(
+'sudarshan','gadiyar',22);
+
+select * from employees;
